e010c9eca58f4f7284f6bb081cc4fc456cf1e709
xavixava <zerezeze@gmail.com>
2022-09-28 11:06:31 +0100

    regression and testing

--- teste.py
+++ teste.py
@@ -1,27 +1,47 @@
 #!/usr/bin/python3
 import numpy as np
 
-fnx = "Xtrain_Regression1.npy"
-fny = "Ytrain_Regression1.npy"
+fnx = "files/Xtrain_Regression1.npy"
+fny = "files/Ytrain_Regression1.npy"
+fnbeta = "files/beta_aprox.npy"
+fnteste = "files/Xtest_Regression1.npy"
+fnytest = "files/y_hat.npy"
 
 def main():
     x = np.load(fnx)
-    y = np.load(fny) 
-    
+    y = np.load(fny)
+     
     if x.shape[0] != y.shape[0]:
         print("Training set has different samples amounts for x and y.")
         exit(0)
 
     n = x.shape[0]
     p = x.shape[1]
-    print("n =", n, "; p =", p)
-    
+        
     xt = np.transpose(x)
     aux = np.linalg.inv(np.matmul(xt, x))
-    print(aux.shape)
-    aux2 = np.matmul(x, y)
-    print(aux2.shape)
+    
+    aux2 = np.matmul(xt, y)
+    
     beta = np.matmul(aux, aux2)
-    print(beta.shape)
+    
+    np.save(fnbeta, beta)
 
-main()
+def teste():
+    beta = np.load(fnbeta)
+    x_test = np.load(fnteste)
+
+    y_hat = np.matmul(x_test, beta)
+    print(y_hat.shape)
+    np.save(fnteste, y_hat)
+
+while True:
+    a = input("b-calculate betas; t-generate testing data, e-exit: ")
+    if a == "b":
+        main()
+    elif a == "t":
+        teste()
+    elif a == "e":
+        exit(0)
+    else:
+        print("Please follow the instructions!")
