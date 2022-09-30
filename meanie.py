534cbd760d3b6f977aa7fe259461a7e8877ed022
xavixava <zerezeze@gmail.com>
2022-09-30 12:24:12 +0100

    added evaluation

--- meanie.py
+++ meanie.py
@@ -1,47 +1,59 @@
 #!/usr/bin/python3
 import numpy as np
+from sklearn.metrics import mean_squared_error as mse
+from sklearn.metrics import r2_score as r2
 
 fnx = "files/Xtrain_Regression1.npy"
 fny = "files/Ytrain_Regression1.npy"
-fnbeta = "files/beta_aprox.npy"
+fnbeta = "results_mean/beta_aprox.npy"
 fnteste = "files/Xtest_Regression1.npy"
-fnytest = "files/y_hat.npy"
+fnytest = "results_mean/y_hat.npy"
 
 def main():
     x = np.load(fnx)
     y = np.load(fny)
-     
+    
+    aux = np.mean(x, axis=0)
+    aux2 = np.broadcast_to(aux, (100, 10))
+    x = x-aux2 
+
+    aux = np.mean(y)
+    y = y - aux
+
     if x.shape[0] != y.shape[0]:
         print("Training set has different samples amounts for x and y.")
         exit(0)
 
-    n = x.shape[0]
-    p = x.shape[1]
+    x_train = x[:-20]
+    x_test = x[-20:]
+
+    y_train = y[:-20]
+    y_test = y[-20:]
         
-    xt = np.transpose(x)
-    aux = np.linalg.inv(np.matmul(xt, x))
+    xt = np.transpose(x_train)
+    aux = np.linalg.inv(np.matmul(xt, x_train))
     
-    aux2 = np.matmul(xt, y)
+    aux2 = np.matmul(xt, y_train)
     
     beta = np.matmul(aux, aux2)
     
     np.save(fnbeta, beta)
+    
+    y_hat = np.matmul(x_test, beta)
 
-def teste():
-    beta = np.load(fnbeta)
+    print("Mean Squared Error = %2f" % mse(y_test, y_hat))
+    print("Coefficient of determination = %2f" % r2(y_test, y_hat)) 
+
     x_test = np.load(fnteste)
 
     y_hat = np.matmul(x_test, beta)
-    print(y_hat.shape)
-    #print(y_hat)
     np.save(fnytest, y_hat)
+
 
 while True:
     a = input("b-calculate betas; t-generate testing data, e-exit: ")
     if a == "b":
         main()
-    elif a == "t":
-        teste()
     elif a == "e":
         exit(0)
     else:
---
+++ sci.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+import numpy as np
+from sklearn import linear_model
+from sklearn.metrics import mean_squared_error as mse
+from sklearn.metrics import r2_score as r2
+
+fnx = "files/Xtrain_Regression1.npy"
+fny = "files/Ytrain_Regression1.npy"
+fnbeta = "results_sci/beta_aprox.npy"
+fnteste = "files/Xtest_Regression1.npy"
+fnytest = "results_sci/y_hat.npy"
+
+def main():
+    x = np.load(fnx)
+    y = np.load(fny)
+    
+    reg = linear_model.LinearRegression()
+
+    if x.shape[0] != y.shape[0]:
+        print("Training set has different samples amounts for x and y.")
+        exit(0)
+    
+    x_train = x[:-20]
+    x_test = x[-20:]
+
+    y_train = y[:-20]
+    y_test = y[-20:]
+
+    reg.fit(x_train, y_train)
+    
+    np.save(fnbeta, reg.coef_)
+    
+     
+    y_hat = reg.predict(x_test)
+    print("Mean Squared Error = %2f" % mse(y_test, y_hat))
+    print("Coefficient of determination = %2f" % r2(y_test, y_hat))
+
+    x_test = np.load(fnteste)
+    y_hat = reg.predict(x_test)
+    
+    np.save(fnytest, y_hat)
+
+main()
