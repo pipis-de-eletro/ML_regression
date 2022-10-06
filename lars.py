cb46822c4723d1df2d2f54c0130ea034bac91834
xavixava <zerezeze@gmail.com>
2022-10-07 00:02:44 +0100

    final commit

--- lars.py
+++ lars.py
@@ -3,19 +3,22 @@ import numpy as np
 from sklearn import linear_model
 from sklearn.metrics import mean_squared_error as mse
 from sklearn.metrics import r2_score as r2
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
 
 fnx = "files/Xtrain_Regression1.npy"
 fny = "files/Ytrain_Regression1.npy"
-fnbeta = "files/ridge/beta_aprox.npy"
+fnbeta = "files/lasso/beta_aprox.npy"
 fnteste = "files/Xtest_Regression1.npy"
-fnytest = "files/ridge/y_hat.npy"
+fnytest = "files/lasso/y_hat.npy"
 
 def main():
     x = np.load(fnx)
     y = np.load(fny)
-    
-    #reg = linear_model.RidgeCV(alphas=np.linspace(0.09, 0.13, 1000))
-    reg = linear_model.Ridge(alpha=0.12)
+
+
+    reg = make_pipeline(StandardScaler(), linear_model.LassoLarsCV())
+
     if x.shape[0] != y.shape[0]:
         print("Training set has different samples amounts for x and y.")
         exit(0)
@@ -26,12 +29,8 @@ def main():
     y_train = y[:-20]
     y_test = y[-20:]
 
-    reg.fit(x_train, y_train)
+    reg.fit(x_train, y_train.ravel())
     
-    np.save(fnbeta, reg.coef_)
-    
-    #print(reg.alpha_)
-    print(reg.intercept_) 
     y_hat = reg.predict(x_test)
     print("Mean Squared Error = %2f" % mse(y_test, y_hat))
     print("Coefficient of determination = %2f" % r2(y_test, y_hat))
--- lasso.py
+++ lasso.py
@@ -14,12 +14,29 @@ def main():
     x = np.load(fnx)
     y = np.load(fny)
     
-    #reg = linear_model.LassoCV(alphas=np.linspace(0.002, 0.004, 10))
-    reg = linear_model.Lasso(alpha=0.003)
+    '''
+    ========================================================================================================================
+    The first two regressions were to aproximate alpha(with more precision) values, commented so they won't be used anymore
+    Should try to automate it later
+    ======================================================================================================================== 
+    '''
+
+    #reg = linear_model.LassoCV(alphas=np.logspace(-13, 13, 6))
+    #reg = linear_model.LassoCV(alphas=np.linspace(0.0005, 0.005, 20))
+    reg = linear_model.LassoCV(alphas=np.linspace(0.002, 0.004, 4))
+    
     if x.shape[0] != y.shape[0]:
         print("Training set has different samples amounts for x and y.")
         exit(0)
-    
+    '''
+    =========================================================================
+    We separate the training set in a virtual training and testing set, 
+    this is done so we can evaluate the model against overfitting and 
+    compare it against other models
+    I know this might not be necessary since we have cross-validation
+    but since we can retrain it later w/ all the data, we decided to do it
+    =========================================================================
+    ''' 
     x_train = x[:-20]
     x_test = x[-20:]
 
@@ -28,16 +45,24 @@ def main():
 
     reg.fit(x_train, y_train.ravel())
     
-    np.save(fnbeta, reg.coef_)
+    #np.save(fnbeta, reg.coef_) # non-necessary
     
-    #print(reg.alpha_)
+    print(reg.alpha_)
      
     y_hat = reg.predict(x_test)
     print("Mean Squared Error = %2f" % mse(y_test, y_hat))
     print("Coefficient of determination = %2f" % r2(y_test, y_hat))
+    
+    final_model = linear_model.Lasso(alpha=reg.alpha_)
+    final_model.fit(x, y)
+
+    print("MSE: %2f" % mse(y, final_model.predict(x)))
+    print("Score: %2f" % final_model.score(x, y))
 
     x_test = np.load(fnteste)
-    y_hat = reg.predict(x_test)
+    y_hat = final_model.predict(x_test)
+    
+    
     
     np.save(fnytest, y_hat)
 
